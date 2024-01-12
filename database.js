a72e0c5f9942b4a3407a8bc5570a90514efcfcee
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-01-12 00:15:20 -0500

    Actualización de la base de datos

--- database.js
+++ database.js
@@ -2,15 +2,29 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
+// export const sequelize = new Sequelize(
+//   "db_rest",
+//   "postgres", 
+//   "9iZy90V7vB0PLQfrGXnO", 
+//   {
+//    host: "containers-us-west-91.railway.app",
+//    logging: false,
+//    dialect: "postgres",
+//    port: 7261
+//  }  
+// );
+
 export const sequelize = new Sequelize(
   "db_rest",
-  "postgres", 
-  "9iZy90V7vB0PLQfrGXnO", 
-  {
-   host: "containers-us-west-91.railway.app",
-   logging: false,
-   dialect: "postgres",
-   port: 7261
- }  
+   "postgres", 
+   "65aCCbBE46ccE4cFD4GBC4b5E54F4CgB", 
+   {
+    host: "monorail.proxy.rlwy.net",
+    logging: false,
+    dialect: "postgres",
+    port: 12149
+  } 
 );
+
+
 
